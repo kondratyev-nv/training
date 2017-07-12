98107e87b632d3a6c9cc2ba6ee063e10ac902a5d
Nikolay Kondratyev <kondratyev.nv@gmail.com>
2017-07-12 14:21:19 +0700

    Small refactoring

--- java/src/main/java/ru/nk/training/BinaryTreeFlattener.java
+++ java/src/main/java/ru/nk/training/BinaryTreeFlattener.java
@@ -16,18 +16,18 @@ public class BinaryTreeFlattener {
 
     public <T> List<BinaryTreeNode<T>> flatten(BinaryTreeNode<T> root) {
         List<BinaryTreeNode<T>> list = new ArrayList<>();
-        int maxDepth = heightFinder.depth(root);
-        Queue<NodeWithLevel<T>> nodesQueue = new LinkedList<NodeWithLevel<T>>() {
+        int maxLevel = heightFinder.depth(root);
+        Queue<NodeWithLevel<T>> remaining = new LinkedList<NodeWithLevel<T>>() {
             {
-                add(new NodeWithLevel<>(root, 1));
+                add(NodeWithLevel.from(root, 1));
             }
         };
-        while (!nodesQueue.isEmpty()) {
-            NodeWithLevel<T> nwl = nodesQueue.poll();
+        while (!remaining.isEmpty()) {
+            NodeWithLevel<T> nwl = remaining.poll();
             list.add(nwl.node);
-            if (nwl.level < maxDepth) {
-                nodesQueue.add(new NodeWithLevel<>(nwl.node != null ? nwl.node.left : null, nwl.level + 1));
-                nodesQueue.add(new NodeWithLevel<>(nwl.node != null ? nwl.node.right : null, nwl.level + 1));
+            if (nwl.level < maxLevel) {
+                remaining.add(NodeWithLevel.from(nwl.left(), nwl.level + 1));
+                remaining.add(NodeWithLevel.from(nwl.right(), nwl.level + 1));
             }
         }
         return list;
@@ -37,9 +37,21 @@ public class BinaryTreeFlattener {
         public final BinaryTreeNode<T> node;
         public final int level;
 
-        public NodeWithLevel(BinaryTreeNode<T> node, int level) {
+        public static <T> NodeWithLevel<T> from(BinaryTreeNode<T> node, int level) {
+            return new NodeWithLevel<>(node, level);
+        }
+
+        private NodeWithLevel(BinaryTreeNode<T> node, int level) {
             this.node = node;
             this.level = level;
+        }
+
+        public BinaryTreeNode<T> left() {
+            return node != null ? node.left : null;
+        }
+
+        public BinaryTreeNode<T> right() {
+            return node != null ? node.right : null;
         }
     }
 }
