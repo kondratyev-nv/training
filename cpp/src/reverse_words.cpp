a8db9649d8934bebeff683df33c925ba70cc956b
Nikolay Kondratyev <kondratyev.nv@gmail.com>
2017-07-18 21:21:59 +0700

    Improve solution for reverse words, fix c++ build in vscode

--- cpp/src/reverse_words.cpp
+++ cpp/src/reverse_words.cpp
@@ -1,30 +1,30 @@
 #include "reverse_words.hpp"
 
-int find_next_space(const std::string& s, int from) {
-    for (int i = from; i < s.length(); ++i) {
-        if (s[i] == ' ') {
-            return i;
-        }
-    }
-    return s.length();
+#include <algorithm>
+
+using namespace std;
+
+size_t find_next_word(const string& sentence, size_t from) {
+    size_t index = sentence.find_first_not_of(' ', from);
+    return index == string::npos ? sentence.length() : index;
 }
 
-void reverse_word(std::string& s, int from, int to) {
+size_t find_next_space(const string& sentence, size_t from) {
+    size_t index = sentence.find_first_of(' ', from);
+    return index == string::npos ? sentence.length() : index;
+}
+
+void reverse_word(string& sentence, size_t from, size_t to) {
     while (from < to) {
-        char letter = s[to];
-        s[to] = s[from];
-        s[from] = letter;
-
-        from++;
-        to--;
+        swap(sentence[to--], sentence[from++]);
     }
 }
 
-void reverse_words(std::string& s) {
-    int word_start = 0;
-    while (word_start < s.length()) {
-        int word_end = find_next_space(s, word_start);
-        reverse_word(s, word_start, word_end - 1);
-        word_start = word_end + 1;
+void reverse_words(string& sentence) {
+    size_t word_start = find_next_word(sentence, 0);
+    while (word_start < sentence.length()) {
+        size_t word_end = find_next_space(sentence, word_start);
+        reverse_word(sentence, word_start, word_end - 1);
+        word_start = find_next_word(sentence, word_end);
     }
 }
