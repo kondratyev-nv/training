1579129a4fd90fd3f13a11cbfe5422efccbfbbd2
Nikolay Kondratyev <kondratyev.nv@gmail.com>
2018-01-30 15:19:38 +0300

    Rebuild c_cpp_properties.json, format code

--- cpp/include/rectangles_intersect.hpp
+++ cpp/include/rectangles_intersect.hpp
@@ -3,6 +3,6 @@
 
 #include "rectangle.hpp"
 
-bool rectangles_intersect(const rectangle &r1, const rectangle &r2);
+bool rectangles_intersect(const rectangle& r1, const rectangle& r2);
 
 #endif  // RECTANGLES_INTERSECT_HPP
