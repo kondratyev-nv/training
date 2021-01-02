74f1a8cdb822d9f8efb921ac15dbe264a8f4c344
Nikolay Kondratyev <4085884+kondratyev-nv@users.noreply.github.com>
2021-01-02 12:35:30 +0100

    Another attempt to fix C++ coverage

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+BUILD_TYPE=${BUILD_TYPE:-"Debug"}
+
 mkdir -p build && \
   cd build && \
-    cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" .. && \
-      cmake --build . && \
-        ctest --parallel 4 --timeout 10 --output-on-failure
+    cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE .. && \
+      cmake --build . --config $BUILD_TYPE && \
+        ctest -C $BUILD_TYPE --parallel 4 --timeout 10 --output-on-failure
